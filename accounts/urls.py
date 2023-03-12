2151b5e40b279da6194e86f3b11335b0ea69705e
Sajjaddoodabi <sajjaddoodabi55@gmail.com>
2023-03-12 10:53:04 +0330

    add user view and url

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,10 +3,11 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
-from .views import UserRegisterView
+from .views import UserRegisterView, UserView
 
 urlpatterns = [
     path('register/', UserRegisterView.as_view(), name='register'),
+    path('user/', UserView.as_view(), name='register'),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render
-from rest_framework.generics import CreateAPIView
+from rest_framework.exceptions import AuthenticationFailed
+from rest_framework.generics import CreateAPIView, RetrieveAPIView
 
 from .models import User
-from .serializers import UserRegisterSerializer
+from .serializers import UserRegisterSerializer, UserSerializer
 
 
 class UserRegisterView(CreateAPIView):
     serializer_class = UserRegisterSerializer
     queryset = User.objects.all()
 
+
+class UserView(RetrieveAPIView):
+    serializer_class = UserSerializer
+
+    def get_object(self):
+        if self.request.user.is_authenticated:
+            return self.request.user
+        raise AuthenticationFailed('unauthenticated')
