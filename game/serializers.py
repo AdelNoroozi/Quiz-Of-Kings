7b95aaea71d847100bf82108081ec58c7c4b6f29
Adel <adel13no@gmail.com>
2023-03-13 21:17:27 +0330

    -small modifications in game serializers

--- game/serializers.py
+++ game/serializers.py
@@ -8,7 +8,7 @@ class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = ('id', 'name', 'is_active')
-        read_only_fields = ('id',)
+        read_only_fields = ('id','is_active')
 
 
 class ChoiceMiniSerializer(serializers.ModelSerializer):
@@ -20,16 +20,16 @@ class ChoiceMiniSerializer(serializers.ModelSerializer):
 
 class QuestionSerializer(serializers.ModelSerializer):
     category = CategorySerializer(many=False)
-    created_by = PlayerMiniSerializer(many=False)
-    confirmed_by = AdminSerializer(many=False)
-    choices = ChoiceMiniSerializer(many=True)
+    created_by = PlayerMiniSerializer(many=False, read_only=True)
+    confirmed_by = AdminSerializer(many=False, read_only=True)
+    choices = ChoiceMiniSerializer(many=True, read_only=True)
 
     class Meta:
         model = Question
         fields = (
             'id', 'category', 'text', 'choices', 'answered_count', 'created_by', 'confirmed_by', 'created_at',
             'confirmed_at', 'likes', 'dislikes')
-        read_only_fields = ('id',)
+        read_only_fields = ('id', 'answered_count', 'confirmed_by', 'created_at', 'confirmed_at', 'likes', 'dislikes')
 
 
 class QuestionMiniSerializer(serializers.ModelSerializer):
@@ -47,35 +47,37 @@ class ChoiceSerializer(serializers.ModelSerializer):
     class Meta:
         model = Choice
         fields = ('id', 'question', 'text', 'chosen_count', 'is_correct')
-        read_only_fields = ('id',)
+        read_only_fields = ('id', 'chosen_count')
 
 
 class MatchSerializer(serializers.ModelSerializer):
-    starter_player = PlayerMiniSerializer(many=False)
-    joining_player = PlayerMiniSerializer(many=False)
-    selected_categories = CategorySerializer(many=True)
+    starter_player = PlayerMiniSerializer(many=False, read_only=True)
+    joining_player = PlayerMiniSerializer(many=False, read_only=True)
+    selected_categories = CategorySerializer(many=True, read_only=True)
 
     class Meta:
         model = Match
         fields = (
             'id', 'starter_player', 'joining_player', 'starter_player_score', 'joining_player_score',
             'selected_categories', 'status', 'created_at', 'modified_at', 'turn', 'expires_at', 'rounds_played')
-        read_only_fields = ('id',)
+        read_only_fields = (
+            'id', 'starter_player_score', 'joining_player_score', 'status', 'created_at', 'modified_at', 'turn',
+            'expires_at', 'rounds_played')
 
 
 class MatchMiniSerializer(serializers.ModelSerializer):
-    starter_player = PlayerMiniSerializer(many=False)
-    joining_player = PlayerMiniSerializer(many=False)
+    starter_player = PlayerMiniSerializer(many=False, read_only=True)
+    joining_player = PlayerMiniSerializer(many=False, read_only=True)
 
     class Meta:
         model = Match
         fields = (
             'id', 'starter_player', 'joining_player', 'created_at')
-        read_only_fields = ('id',)
+        read_only_fields = ('id', 'created_at')
 
 
 class PlayerAnswerSerializer(serializers.ModelSerializer):
-    player = PlayerMiniSerializer(many=False)
+    player = PlayerMiniSerializer(many=False, read_only=True)
     match = MatchMiniSerializer(many=False)
     question = QuestionMiniSerializer(many=False)
     answer = ChoiceMiniSerializer(many=False)
