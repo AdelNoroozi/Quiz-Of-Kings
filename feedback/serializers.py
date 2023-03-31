81d406333e7f8e1a7581038a78f00fbe268f7feb
Sajjaddoodabi <sajjaddoodabi55@gmail.com>
2023-03-31 15:10:18 +0430

    modification

--- feedback/serializers.py
+++ feedback/serializers.py
@@ -4,6 +4,7 @@ from .models import LikeOrDislike
 
 class LikeOrDislikeSerializer(serializers.Serializer):
     user = serializers.CharField(source='user.user.username', read_only=True)
+    question = serializers.CharField(source='question.text', read_only=True)
 
     class Meta:
         model = LikeOrDislike
