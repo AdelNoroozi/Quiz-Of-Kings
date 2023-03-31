a0efda1c3f0b6d798d4a929f44a1968127f7dfdb
Sajjaddoodabi <sajjaddoodabi55@gmail.com>
2023-03-31 15:06:48 +0430

    modified like or dislike view

--- feedback/urls.py
+++ feedback/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import LikeOrDislikeView
 
 urlpatterns = [
-    path('like-dislike', LikeOrDislikeView.as_view(), name='like_dislike')
+    path('like-dislike/', LikeOrDislikeView.as_view(), name='like_dislike')
 ]
--- feedback/views.py
+++ feedback/views.py
@@ -4,6 +4,7 @@ from rest_framework.exceptions import AuthenticationFailed
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from accounts.models import Player
 from game.models import Question
 from .models import LikeOrDislike
 from .serializers import LikeOrDislikeSerializer
@@ -17,32 +18,43 @@ class LikeOrDislikeView(APIView):
             if user.is_anonymous:
                 raise AuthenticationFailed('unauthenticated')
 
-            question_id = serializer.data['question']
-            feedback_type = serializer.data['type']
+            player = Player.objects.filter(user=user).first()
+            if not player:
+                response = {'detail': 'player NOT found!'}
+                return Response(response)
+
+            try:
+                question_id = request.data['question']
+                feedback_type = request.data['type']
+
+            except Exception as e:
+                response = {'detail': str(e)}
+                return Response(response)
 
             question = Question.objects.filter(id=question_id).first()
             if not question:
                 response = {'detail': 'question NOT found!'}
                 return Response(response)
 
-            if feedback_type != 'L' or feedback_type != 'D':
+            if feedback_type == 'L' or feedback_type == 'D':
+                try:
+                    like_dislike = LikeOrDislike.objects.create(user=player, question=question, type=feedback_type)
+                except Exception as e:
+                    response = {'detail': str(e)}
+                    return Response(response)
+
+                if feedback_type == 'L':
+                    question.likes += 1
+                else:
+                    question.dislikes += 1
+
+                question.save()
+
+                ser = LikeOrDislikeSerializer(like_dislike)
+                return Response(ser.data)
+
+            else:
                 response = {'detail': 'invalid type!'}
                 return Response(response)
-
-            try:
-                like_dislike = LikeOrDislike.objects.create(user=user, question=question, type=feedback_type)
-            except Exception as e:
-                response = {'detail': str(e)}
-                return Response(response)
-
-            if feedback_type == 'L':
-                question.likes += 1
-            else:
-                question.dislikes += 1
-
-            question.save()
-
-            ser = LikeOrDislikeSerializer(like_dislike)
-            return Response(ser.data)
 
         return Response(serializer.errors)
