b3ccafdcfe6b6a70dbfbfe93a3a2faa89dd526c0
Sajjaddoodabi <sajjaddoodabi55@gmail.com>
2023-03-31 16:25:27 +0430

    modified feedback admin

--- feedback/admin.py
+++ feedback/admin.py
@@ -1,4 +1,22 @@
 from django.contrib import admin
 from .models import *
 
-admin.site.register(LikeOrDislike)
+
+@admin.register(LikeOrDislike)
+class LikeOrDislikeAdmin(admin.ModelAdmin):
+    list_display = ['user', 'question', 'type']
+    list_filter = ['type']
+
+
+@admin.register(Report)
+class ReportAdmin(admin.ModelAdmin):
+    list_display = ['user', 'question', 'reason', 'admin', 'status']
+    list_editable = ['status', ]
+    list_filter = ['status', ]
+
+
+@admin.register(ReportReason)
+class ReportReasonAdmin(admin.ModelAdmin):
+    list_display = ['reason', 'is_active']
+    list_editable = ['is_active', ]
+    list_filter = ['is_active', ]
